a7f5ae7f781a314e0c346527db4bee52c93b68b8
Ashika-Sivan <ashikasivan07@gmail.com>
2025-10-15 15:13:09 +0530

    Time: 0 ms (100%), Space: 57.1 MB (15.18%) - LeetHub

---
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    // console.log(matrix[0].length)
+    for(let i=1;i<matrix.length;i++){
+        for(let j=1;j<matrix[i].length;j++){
+            if(matrix[i][j]!==matrix[i-1][j-1]){
+                return false
+            }
+            
+        }
+    }
+    return true
+};
