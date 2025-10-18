6422227d3aae7b0acd91719690f1146df786327f
Ashika-Sivan <ashikasivan07@gmail.com>
2025-10-18 17:22:43 +0530

    Time: 1 ms (45.84%), Space: 55.4 MB (76.09%) - LeetHub

---
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var gcdOfOddEvenSums = function(n) {
+    // let odd=[]
+    // let even=[]
+    // for(let i=1;i<=n*2;i++){
+    //     if(i%2===0){
+    //         odd.push(i)
+
+    //     }else{
+    //         even.push(i)
+
+    //     }
+    // }
+    
+    // let sum1=odd.reduce((acc,curr)=>acc+curr,0)
+    // let sum2=even.reduce((acc,curr)=>acc+curr,0)
+return n
+};
