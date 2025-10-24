8a42765f10a9a898909f1649b1c8a54e241aaddc
Ashika-Sivan <ashikasivan07@gmail.com>
2025-10-24 16:16:53 +0530

    Time: 5 ms (82.47%), Space: 56.3 MB (53.61%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let x=0
+    let y=0
+    for(let i of moves){
+        if(i==='U')y++
+        else if(i==='D')y--
+        else if(i=='L')x++
+        else if(i=='R')x--
+        
+        
+    }
+    
+
+    return x===0&&y===0
+};
