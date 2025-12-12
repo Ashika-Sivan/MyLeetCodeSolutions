06cf0e223f7c6f774bd3dcefbc74b793e15ce71e
Ashika-Sivan <ashikasivan07@gmail.com>
2025-12-12 09:42:44 +0530

    Time: 0 ms (100%), Space: 54 MB (45.46%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if(n<=2)return n
+    let one=1
+    let two=2
+
+    for(let i=3;i<=n;i++){
+        let temp=one+two
+        one=two
+        two=temp
+    }
+    
+   return two
+};
