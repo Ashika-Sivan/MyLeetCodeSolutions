48290f5e8795828dfb37217b19193ade786b7c54
Ashika-Sivan <ashikasivan07@gmail.com>
2025-12-22 17:50:00 +0530

    Time: 1 ms (100%), Space: 56.1 MB (100%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var mirrorDistance = function(n) {
+    let rev=n.toString().split('').reverse().map((x)=>Number(x)).join('')
+    let res=Math.abs(n-rev)
+    return res
+    
+};
