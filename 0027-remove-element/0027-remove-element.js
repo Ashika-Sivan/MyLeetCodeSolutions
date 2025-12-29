5a6fdf9177bff7e549aaa0a700ea17cb87d3fbdd
Ashika-Sivan <ashikasivan07@gmail.com>
2025-12-29 23:23:13 +0530

    Time: 2 ms (4.85%), Space: 56.2 MB (5.2%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+       for(let i=0;i<nums.length;i++){
+        if(nums[i]===val){
+            for(let j=i;j<nums.length;j++){
+                nums[j]=nums[j+1]
+            }
+            i--
+            nums.length--
+        }
+    
+    }
+   console.log(nums)
+
+    
+};
