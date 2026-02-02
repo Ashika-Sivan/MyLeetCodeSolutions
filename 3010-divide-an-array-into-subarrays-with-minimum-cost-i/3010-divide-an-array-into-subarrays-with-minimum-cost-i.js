02765431e385b7ecb466c752ed5e23b44b4c7de7
Ashika-Sivan <ashikasivan07@gmail.com>
2026-02-02 19:17:59 +0530

    Time: 1 ms (88.46%), Space: 56.1 MB (73.08%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.js
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.js
@@ -5,13 +5,13 @@
 var minimumCost = function(nums) {
     let sum=nums[0]
     nums.shift()
-    let sortArr=nums.sort((a,b)=>a-b).slice(0,2)
-    console.log(sortArr)
-    for(let i=0;i<sortArr.length;i++){
-        sum+=sortArr[i]
-    }
+    let sortArr=nums.sort((a,b)=>a-b).slice(0,2).reduce((acc,curr)=>acc+curr,sum)
+    return sortArr
+    // for(let i=0;i<sortArr.length;i++){
+    //     sum+=sortArr[i]
+    // }
 
-   return sum
+//    return sum
     
     
 };
