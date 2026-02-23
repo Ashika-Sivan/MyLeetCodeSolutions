35eeafdc55429ba7dd7f2d932cb3e9c8c5ccc5ad
Ashika-Sivan <ashikasivan07@gmail.com>
2026-02-23 16:52:11 +0530

    Time: 0 ms (100%), Space: 58.5 MB (8.09%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+  
+  let rev=image.map((x)=>x.reverse().map((x)=>x==1?0:1))
+    return rev
+ 
+  
+  
+    
+};
