e6856700ea50c00b95bd24bd036f33a8a4f7d554
Ashika-Sivan <ashikasivan07@gmail.com>
2026-03-18 10:18:08 +0530

    Time: 3 ms (8.29%), Space: 56.7 MB (25.05%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.js
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -2,32 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var scoreOfString = function(s) {
-    let arr=[]
-    for(let i=0;i<s.length;i++){
+var scoreOfString = function (s) {
+    let arr = []
+    for (let i = 0; i < s.length; i++) {
         arr.push(s[i].charCodeAt())
     }
-    
-   let p=[]
-   let sum=0
-    for(let i=0;i<arr.length;i++){
-        for(let j=i+1;j<arr.length;j++){
-             p.push(Math.abs(arr[i]-arr[j]))
-             break
-            
+    let p = []
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            p.push(Math.abs(arr[i] - arr[j]))
+            break
+
         }
-           
-        
     }
-     return p.reduce((acc,curr)=>acc+curr,0)
-     
-   
-   
-    
-    
-
-
-
-    
-    
+    return p.reduce((acc, curr) => acc + curr, 0)
 };
