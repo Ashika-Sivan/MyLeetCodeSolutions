5230147c25d4a4b822b78ba9c753a5e7d1099cfe
Ashika-Sivan <ashikasivan07@gmail.com>
2026-03-23 17:58:32 +0530

    Time: 0 ms (100%), Space: 53.7 MB (65.23%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num,count=0) {
+    if(num===0){
+        return count
+    }
+
+    if(num%2===0){
+        return numberOfSteps(num/2,count+1)
+    }else{
+        return numberOfSteps(num-1,count+1)
+    }
+ 
+};
