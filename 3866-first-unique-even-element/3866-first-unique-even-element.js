b155531314de432ea5265e49dfd6066258ce72aa
Ashika-Sivan <ashikasivan07@gmail.com>
2026-05-10 19:35:16 +0530

    Time: 0 ms (100%), Space: 56.1 MB (83.09%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstUniqueEven = function(nums) {
+    let res=[]
+    let seen=new Set()
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]%2===0){
+            res.push(nums[i])
+
+        }
+    }
+
+    let final= res.filter((x)=>res.indexOf(x)===res.lastIndexOf(x))
+    if(final.length===0){
+        return -1
+    }else{
+        return final[0]
+    }
+  
+    
+
+
+};
