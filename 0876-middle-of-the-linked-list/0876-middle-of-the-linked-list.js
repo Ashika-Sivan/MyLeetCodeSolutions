1f0636062a560baeccc977c84f9fcf77c60d41bf
Ashika-Sivan <ashikasivan07@gmail.com>
2026-05-18 19:05:35 +0530

    Time: 0 ms (100%), Space: 53.5 MB (58.63%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,15 +10,15 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
+
     let fast=head
     let slow=head
-    while(fast&&fast.next){
+    while(fast && fast.next){
         slow=slow.next
         fast=fast.next.next
-
     }
-    return slow
     
+    return slow
    
 
     
