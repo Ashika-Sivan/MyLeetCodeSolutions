14286b578ae1e8fca699c336903293286adcbab9
Ashika-Sivan <ashikasivan07@gmail.com>
2026-07-15 15:08:05 +0530

    Time: 3 ms (23.94%), Space: 56.2 MB (45.71%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var digitFrequencyScore = function(n) {
+    let splitN=n.toString().split('').map((x)=>Number(x))
+    let freq={}
+    for(let num of splitN){
+        freq[num]=(freq[num]||0)+1
+    }
+    let sum=0
+    for(let num in freq){
+        let res=num*freq[num]
+        sum+=res
+    }
+    return sum
+    
+};
