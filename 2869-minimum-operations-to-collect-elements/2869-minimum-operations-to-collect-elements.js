75694fefb65964e286bc8485d07c6ced0747ba55
Ashika-Sivan <ashikasivan07@gmail.com>
2026-09-15 01:15:53 +0530

    Time: 1 ms (40.63%), Space: 55.2 MB (81.25%) - LeetHub

---
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+     let collection = new Set(), numOfOps = 0;
+
+    while (collection.size !== k) {
+        let pop = nums.pop();
+        if (pop <= k) collection.add(pop);
+        numOfOps += 1;
+    }
+
+    return numOfOps;
+};
